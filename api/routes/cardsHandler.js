d546d4d5c0b62f17dc6b9b3ef997598917f70ae5
Sami Mahjoub <sami-mahjoub@laposte.net>
2018-01-20 17:10:28 +0100

    /getcard fix

--- api/routes/cardsHandler.js
+++ api/routes/cardsHandler.js
@@ -4,21 +4,30 @@ var jwt_handler = require('../controllers/check_jwt');
 router.use(jwt_handler);
 
 router.get('/card', function (req, res, next) {
-    var ID = req.body.id;
+    var player_id = req.decoded_data.Id;
+    console.log("id est", player_id);
     var ret = {
         "status": "200",
         "cards": []
     };
     res.json(ret);
-    var sql = "SELECT * FROM card WHERE Id='"+ID+"'";
+    var sql = 'SELECT card.ID, card.Sprite, card.Name, card.Cost, card.HP, card.ATK, card.Rarity_id,\
+        type_card.Name "Type",\
+        faction.Name "Faction",\
+        card_collection.Number\
+    FROM card\
+    LEFT JOIN card_collection ON card.ID=card_collection.Card_id\
+    LEFT JOIN type_card ON card.Type_card_id=type_card.ID\
+    LEFT JOIN faction ON card.Faction_id=faction.ID\
+    WHERE card_collection.Player_id="'+player_id+'"';
     var query = db.query(sql, function (err, results) {
         if (err){
             throw err;
         }
         else {
             if (results !== ""){
+                console.log(results);
                 ret.cards = results;
-
             }
         }
     });

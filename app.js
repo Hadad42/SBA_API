48c25bd6986e7dde8fc6f7a7b61e2e4d4a666861
Sami Mahjoub <sami-mahjoub@laposte.net>
2018-02-09 20:25:14 +0100

    Allow-Methods GET,PUT,POST,DELETE
    add header to allow GET,PUT,POST,DELETE

--- app.js
+++ app.js
@@ -55,6 +55,7 @@ app.use(function (req, res, next) {
     app.use(cors());
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     next();
 });
 

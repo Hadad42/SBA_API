9b245720c1f7de796171c11549ce50261538b6cf
Sami Mahjoub <sami-mahjoub@laposte.net>
2018-02-14 19:27:49 +0100

    Remove former card folder

--- api/routes/Card/routes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-//Card Handler
-app.get('/cards', require('./checkCardsHandler'));
-app.delete('/cards/dismantle', require('./deleteCardsHandler'));
-
-module.exports = router;
